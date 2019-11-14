2a52e434e7f52009162928dab1647f48a91ad31a
Duskfen <Gabriel@waldviertelblick.at>
2019-11-15 00:03:56 +0100

    starting to write a script for machine learning

---
+++ Maschinelles Lernen/Samples/1.2.1 Bedeutungsanalyse/Pseudocode Samples.js
@@ -0,0 +1,20 @@
+// pseudocode
+let articles = [
+    {
+        label: "positive",
+        data: "The lizard movie was great! I really liked..."
+    },
+    {
+        label: "positive",
+        data: "Awesome lizards! The color green is my fav..."
+    },
+    {
+        label: "negative",
+        data: "Total disaster! I never liked..."
+    },
+    {
+        label: "negative",
+        data: "Worst movie of all time!..."
+    }
+];
+
